2dfddeec28981b00269a0cb792fe9b4ec48abf22
Patrick Longo <ck.longo@bol.com.br>
2019-10-31 17:03:18 -0300

    Finalizado - iniciar desafio.

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,24 +1,5 @@
 import styled, { keyframes, css } from 'styled-components';
 
-export const Container = styled.div`
-  max-width: 700px;
-  background: #fff;
-  border-radius: 4px;
-  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
-  padding: 30px;
-  margin: 80px auto;
-
-  h1 {
-    font-size: 20px;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-
-    svg {
-      margin-right: 10px;
-    }
-  }
-`;
 export const Form = styled.form`
   margin-top: 30px;
   display: flex;
@@ -88,11 +69,12 @@ export const List = styled.ul`
     }
 
     a {
-      font-size: 14px;
-      font-weight: bold;
       text-decoration: none;
-      color: #666;
-      margin-left: 15px;
+      color: #7159c1;
+      font-weight: bold;
+      padding: 8px;
+      border: 2px solid #7159c1;
+      border-radius: 14px;
     }
   }
 `;
